259b9960507dfb6b820cbfe573aaca843d1ac08a
Scienceable <75717978+Scienceable@users.noreply.github.com>
2022-05-14 00:08:16 -0600

    Add files via upload

--- ESP32_code/ESP32_code.ino
+++ ESP32_code/ESP32_code.ino
@@ -1,12 +1,6 @@
 #include <ESP32Servo.h>
 #include <TinyGPS++.h>
 
-Servo servo1;
-
-int servo1Pin = 18;
-
-int minUs = 1000;
-int maxUs = 2000;
 
 static const int RXPin = 16, TXPin = 17;
 static const uint32_t GPSBaud = 9600;
@@ -35,7 +29,7 @@ TinyGPSPlus gps;
 #define API_KEY ""
 
 /* 3. Define the project ID */
-#define FIREBASE_PROJECT_ID ""
+#define FIREBASE_PROJECT_ID science-club-ecchs"
 
 /* 4. Define the user Email and password that alreadey registerd or added in your project */
 #define USER_EMAIL ""
@@ -49,8 +43,6 @@ FirebaseConfig config;
 
 unsigned long dataMillis = 0;
 int count = 0;
-
-int times = 0;
 
 //The Firestore payload upload callback function
 void fcsUploadCallback(CFS_UploadStatusInfo info)
@@ -78,8 +70,18 @@ void fcsUploadCallback(CFS_UploadStatusInfo info)
 }
 
 String isActivatedcontent = "";
+String depthActivatedContent = "";
+int depthActivated;
 bool isActivated = false;
 bool isConfirmed = false;
+bool motorActivated = false;
+float realDepth;
+
+
+const int motorPin = 2;
+const int LEDPin = 23;
+const int sensorPin = 35;
+const int buttonPin = 19;
 
 void setup()
 {
@@ -90,12 +92,11 @@ void setup()
   Serial.begin(115200);
   Serial1.begin(GPSBaud, SERIAL_8N1, RXPin, TXPin);
 
-  servo1.setPeriodHertz(50);
 
-
-  pinMode(2, OUTPUT);
-  pinMode(23, OUTPUT);
-  pinMode(22, INPUT);
+  pinMode(LEDPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(motorPin, OUTPUT);
+  pinMode(sensorPin, INPUT);
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
@@ -128,26 +129,38 @@ void setup()
   //For sending payload callback
   //config.cfs.upload_callback = fcsUploadCallback;
 
-  digitalWrite(2, LOW);
 
 }
 
 void loop() {
-  digitalWrite(2, LOW);
-  servo1.attach(servo1Pin, minUs, maxUs);
   if ((isActivated == false) and (isConfirmed == false)) {
-    digitalWrite(23, LOW);
-    servo1.write(0);
-    servo2.write(0);
+    int stateButton = digitalRead(buttonPin);
+    if (stateButton == 1) {
+      while (stateButton == 1) {
+        digitalWrite(LEDPin, HIGH);
+        stateButton = digitalRead(buttonPin);
+      }
+    }
+    digitalWrite(LEDPin, LOW);
+    digitalWrite(motorPin, LOW);
+    Serial.println((analogRead(sensorPin) - 238));
+
+    realDepth = (analogRead(sensorPin) - 238) / 12.86 * 6895 / 9.81 / 1000;
+    if (realDepth < 0) {
+      realDepth = 0;
+    }
+    Serial.println(realDepth);
     if (Firebase.ready())
     {
+      delay(1000);
       String documentPathGet = "projects/1";
       String isActivatedGet = "isActivated";
+      String depthActivatedGet = "depthActivated";
 
       //If the document path contains space e.g. "a b c/d e f"
       //It should encode the space as %20 then the path will be "a%20b%20c/d%20e%20f"
 
-      Serial.print("Get a document... ");
+      Serial.print("Get 2 documents... ");
 
       if (Firebase.Firestore.getDocument(&fbdo, FIREBASE_PROJECT_ID, "", documentPathGet.c_str(), isActivatedGet.c_str())) {
         isActivatedcontent = fbdo.payload();
@@ -155,65 +168,106 @@ void loop() {
         if (isActivatedcontent.indexOf("false") > 0) {
           isActivated = false;
         } else if (isActivatedcontent.indexOf("true") > 0) {
-          isConfirmed = true;
+          if ((isActivated == false) and (isConfirmed == false)) {
+            Serial.println("Activated, start blinking LED");
+            for (int times = 0; times < 9; times++) {
+              digitalWrite(LEDPin, HIGH);
+
+              delay (1000);
+              digitalWrite(LEDPin, LOW);
+              delay (1000);
+              Serial.println(times);
+              Serial.println("Activated, started blinking LED");
+              stateButton = digitalRead(buttonPin);
+              if (stateButton == 1) {
+                if (Firebase.ready())
+                {
+                  Serial.println(stateButton);
+                  Serial.println("Cancelled");
+                  digitalWrite(2, LOW);
+
+                  FirebaseJson content;
+
+                  String documentPath = "projects/1";
+
+                  String doc_path = "projects/";
+                  doc_path += FIREBASE_PROJECT_ID;
+                  doc_path += "/databases/(default)/documents/coll_id/doc_id"; //coll_id and doc_id are your collection id and document id
+
+                  content.set("fields/isActivated/booleanValue", false);
+
+                  Serial.print("Update a document... ");
+
+                  if (Firebase.Firestore.patchDocument(&fbdo, FIREBASE_PROJECT_ID, "" /* databaseId can be (default) or empty */, documentPath.c_str(), content.raw(), "isActivated"))
+                    Serial.printf("ok\n%s\n\n", fbdo.payload().c_str());
+                  else
+                    Serial.println(fbdo.errorReason());
+                  digitalWrite(LEDPin, HIGH);
+                  delay(2000);
+                  digitalWrite(LEDPin, LOW);
+                  delay(250);
+                  digitalWrite(LEDPin, HIGH);
+                  delay(250);
+                  digitalWrite(LEDPin, LOW);
+                  delay(250);
+                  digitalWrite(LEDPin, HIGH);
+                  delay(250);
+                  digitalWrite(LEDPin, LOW);
+
+                  isConfirmed == false;
+                  isActivated == false;
+                  times = 100;
+                  count = 0;
+                }
+              }
+              count++;
+            }
+            if (count == 9) {
+              isActivated = true;
+              count = 0;
+            }
+          }
+          Serial.println(isActivated);
         } else {
-          isActivated = false;
+          Serial.println(fbdo.errorReason());
         }
-
-        Serial.println(isActivated);
       }
-      else
+
+      if (Firebase.Firestore.getDocument(&fbdo, FIREBASE_PROJECT_ID, "", documentPathGet.c_str(), depthActivatedGet.c_str())) {
+        depthActivatedContent = fbdo.payload();
+        Serial.println(depthActivatedContent);
+        Serial.println(depthActivatedContent.substring(depthActivatedContent.indexOf("integerValue") + 16, depthActivatedContent.indexOf("integerValue") + 18));
+        depthActivated = (depthActivatedContent.substring(depthActivatedContent.indexOf("integerValue") + 16, depthActivatedContent.indexOf("integerValue") + 18)).toInt();
+        Serial.println(depthActivated);
+      } else {
         Serial.println(fbdo.errorReason());
-    }
-  } else if ((isConfirmed == true) and (isActivated == false)) {
-    for (times = 0; times < 10; times++) {
-      digitalWrite(23, HIGH);
-      int stateButton = digitalRead(22);
-      if (stateButton == 1) {
-        if (Firebase.ready())
-        {
-          digitalWrite(2, HIGH);
-
-          FirebaseJson content;
-
-          String documentPath = "projects/1";
-
-          String doc_path = "projects/";
-          doc_path += FIREBASE_PROJECT_ID;
-          doc_path += "/databases/(default)/documents/coll_id/doc_id"; //coll_id and doc_id are your collection id and document id
-
-          content.set("fields/isActivated/booleanValue", false);
-
-          Serial.print("Update a document... ");
-
-          if (Firebase.Firestore.patchDocument(&fbdo, FIREBASE_PROJECT_ID, "" /* databaseId can be (default) or empty */, documentPath.c_str(), content.raw(), "isActivated"))
-            Serial.printf("ok\n%s\n\n", fbdo.payload().c_str());
-          else
-            Serial.println(fbdo.errorReason());
-
-          isConfirmed == false;
-          times = 11;
-          break;
-        }
       }
-      delay (1000);
-      digitalWrite(23, LOW);
-      delay (1000);
-      Serial.println(times);
+      if (realDepth > depthActivated) {
+        isActivated = true;
+        isConfirmed = true;
+      }
     }
-    isActivated = true;
-    
-  } else if (isActivated == true) {
+  } else {
+    Serial.println(isConfirmed);
+    Serial.println(isActivated);
+    if (motorActivated == false) {
+      digitalWrite(motorPin, HIGH);
+      delay(30000);
+      digitalWrite(motorPin, LOW);
+      motorActivated = true;
+    }
+    unsigned long start = millis();
+    do
+    {
+      while (Serial1.available())
+        gps.encode(Serial1.read());
+    } while (millis() - start < 1000);
 
-    smartDelay(1000);
     if (millis() > 5000 && gps.charsProcessed() < 10)
       Serial.println(F("No GPS data received: check wiring"));
-    servo1.write(180);
-    servo2.write(180);
 
     if (Firebase.ready())
     {
-      digitalWrite(2, HIGH);
 
       //For the usage of FirebaseJson, see examples/FirebaseJson/BasicUsage/Create.ino
       FirebaseJson content;
@@ -269,15 +323,13 @@ void loop() {
       else
         Serial.println(fbdo.errorReason());
     }
+    int stateButton = digitalRead(buttonPin);
+    if (stateButton == 1) {
+      while (stateButton == 1) {
+        digitalWrite(motorPin, HIGH);
+        stateButton = digitalRead(buttonPin);
+      }
+    }
+    digitalWrite(motorPin, LOW);
   }
-}
-
-static void smartDelay(unsigned long ms)
-{
-  unsigned long start = millis();
-  do
-  {
-    while (Serial1.available())
-      gps.encode(Serial1.read());
-  } while (millis() - start < ms);
 }
